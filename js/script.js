8d6bf39fde980c74072530ba53d5574cc87ad348
Roberto Blasco~ <roberza2202@gmail.com>
2025-03-12 03:13:48 +0100

    ejercicio_resuelto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
- // Aquí tu código
+// Aquí tu código
+
+
+
+
+
+
+const agregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+const listOne = document.createElement('li');
+const listTwo = document.createElement('li');
+const listTree = document.createElement('li');
+
+lista.appendChild(listOne);
+lista.appendChild(listTwo);
+lista.appendChild(listTree);
+
+//lista.insertAdjacentHTML("afterbegin", '<li></li><li></li><li></li>');
+
+agregar.addEventListener('click', function() {
+    let elementoUno = prompt('Introduce el primer elemento');
+    let elementoDos = prompt('Introduce el segundo elemento');
+    let elementoTres = prompt('Introduce el tercer elementos');
+    listOne.innerHTML = elementoUno;
+    listTwo.innerHTML = elementoDos;
+    listTree.innerHTML = elementoTres;
+
+})
+
